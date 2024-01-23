a8baf6a53144723d7fe58a87068bcf682f8e80b5
Nadezhda Klinskaya <npantelyeva@gmail.com>
2024-01-23 23:27:29 +0300

    feat: add function deleting files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,23 @@ let dir = path.join(__dirname, 'files-copy');
 async function copyDir () {
     fs.mkdir (dir, { recursive: true });
 
+    fs.readdir(dir)
+        .then (files => {
+            if (files.length !== 0) {
+                for (let file of files) {
+                    (async () => {
+                        try {
+                            await fs.unlink(path.join(dir, file), (err => {
+                                if (err) console.log(err)}));
+                        } catch {
+                            console.error('Error!');
+                        }
+                    })();
+                }
+            }
+        })
+
+
     fs.readdir(oldDir)
         .then (filenames => {
             for (let filename of filenames) {
